2982e9a26ea6c27fded6e591f5737f5cbcc86886
sucre92 <mgilbertsuka@gmail.com>
2025-07-27 16:47:47 +0200

    Added messagebox

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from turtle import Screen,Turtle
 from food import Food
 from scoreboard import Scoreboard
+from tkinter import messagebox
 
 snake= Snake()
 food=Food()
@@ -29,6 +30,10 @@ while is_game_on:
     if snake.head.distance(food)<15:
         food.refresh()
         scoreboard.increase_score()
+    if snake.head.xcor() < -280 or snake.head.xcor()> 280 \
+            or snake.head.ycor() <-280 or snake.head.ycor()>280:
+        is_game_on=False
+        messagebox.showinfo("You lose")
 
 
 
